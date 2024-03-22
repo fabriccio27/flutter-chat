1ce067fbcdb8b1506c8ee5de87d0c0d68deb4255
fabriccio27 <fabricio_inc27@hotmail.com>
2024-03-22 10:48:39 -0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:chat_updated/screens/welcome_screen.dart';
+import 'package:chat_updated/screens/login_screen.dart';
+import 'package:chat_updated/screens/registration_screen.dart';
+import 'package:chat_updated/screens/chat_screen.dart';
+
+void main() => runApp(FlashChat());
+
+class FlashChat extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+        textTheme: const TextTheme(
+          body1: TextStyle(color: Colors.black54),
+        ),
+      ),
+      home: WelcomeScreen(),
+    );
+  }
+}
