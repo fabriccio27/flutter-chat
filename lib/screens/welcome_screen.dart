aa95429923029378bc9f348867e77c8185f4eaef
fabriccio27 <fabricio_inc27@hotmail.com>
2024-04-01 16:09:51 -0300

    Replace PaddedLongButton in WelcomeScreen

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -1,6 +1,7 @@
 import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:chat_updated/screens/login_screen.dart';
 import 'package:chat_updated/screens/registration_screen.dart';
+import 'package:chat_updated/components/PaddedLongButton.dart';
 import 'package:flutter/material.dart';
 
 class WelcomeScreen extends StatefulWidget {
@@ -67,42 +68,18 @@ class _WelcomeScreenState extends State<WelcomeScreen> with SingleTickerProvider
             const SizedBox(
               height: 48.0,
             ),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 16.0),
-              child: Material(
-                elevation: 5.0,
-                color: Colors.lightBlueAccent,
-                borderRadius: BorderRadius.circular(30.0),
-                child: MaterialButton(
-                  onPressed: () {
-                    Navigator.pushNamed(context, LoginScreen.id);
-                  },
-                  minWidth: 200.0,
-                  height: 42.0,
-                  child: const Text(
-                    'Log In',
-                  ),
-                ),
-              ),
+            PaddedLongButton(
+              color: Colors.lightBlueAccent,
+              label: 'Log In',
+              onPressed: () { Navigator.pushNamed(context, LoginScreen.id); }
             ),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 16.0),
-              child: Material(
-                color: Colors.blueAccent,
-                borderRadius: BorderRadius.circular(30.0),
-                elevation: 5.0,
-                child: MaterialButton(
-                  onPressed: () {
+            PaddedLongButton(
+              onPressed: () {
                     Navigator.pushNamed(context, RegistrationScreen.id);
                   },
-                  minWidth: 200.0,
-                  height: 42.0,
-                  child: const Text(
-                    'Register',
-                  ),
-                ),
-              ),
-            ),
+              color: Colors.blueAccent,
+              label: 'Register'
+            )
           ],
         ),
       ),
