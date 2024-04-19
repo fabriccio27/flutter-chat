eece07a1fbc6331cfddd25a91a33b9dc7d30245b
fabriccio27 <fabricio_inc27@hotmail.com>
2024-04-18 23:05:02 -0300

    Add firebase through firebase cli

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,24 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'package:flutter/material.dart';
 import 'package:chat_updated/screens/welcome_screen.dart';
 import 'package:chat_updated/screens/login_screen.dart';
 import 'package:chat_updated/screens/registration_screen.dart';
 import 'package:chat_updated/screens/chat_screen.dart';
 
-void main() => runApp(FlashChat());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();  // Ensures that you have a Flutter context before initializing Firebase
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );  // Initialize Firebase
+  runApp(FlashChat());
+}
 
 class FlashChat extends StatelessWidget {
+  
   @override
   Widget build(BuildContext context) {
+    
     return MaterialApp(
       home: WelcomeScreen(),
       initialRoute: WelcomeScreen.id,
